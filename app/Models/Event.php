3a26a4bc424a897135d050b01dbad8c604f74455
takaakimatsuda <takaaki-matsuda@hotmail.co.jp>
2024-09-01 16:48:28 +0900

    $guardedとタグとのリレーションシップを定義

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    // マスアサインメントを許可しない属性
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    // タグとのリレーションシップを定義
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'event_tags', 'event_id', 'tag_id');
+    }
 }
