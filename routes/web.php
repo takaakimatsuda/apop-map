7642bf713adc6e3734b4588687b080184ca2bbb1
takaakimatsuda <takaaki-matsuda@hotmail.co.jp>
2024-09-07 17:15:47 +0900

    イベントのルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
     Route::resource('tag', TagController::class);
+
+    Route::resource('events', EventController::class);
 });
 
 require __DIR__ . '/auth.php';
