f409c9bd952deaa52b31824b4dcef3775cf7cd51
takaakimatsuda <takaaki-matsuda@hotmail.co.jp>
2024-09-16 15:14:00 +0900

    イベントシーダーの修正

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -19,19 +19,16 @@ class EventSeeder extends Seeder
 
             DB::table('events')->insert([
                 'event_id' => $i,
-                'user_id' => 1,  // 全て同じユーザーとして挿入する場合
+                'user_id' => rand(1, 30),  // 全て同じユーザーとして挿入する場合
                 'title' => 'イベント' . $i,
                 'image_url' => NULL,
-                'dance_genre' => 'POP',
-                'region' => '東京',
                 'venue_name' => '会場' . $i,
-                'venue_address' => '東京都の会場' . $i,
+                'venue_address' => '会場の住所' . $i,
                 'description' => 'イベントの説明文' . $i,
                 'reference_url' => NULL,
                 'date' => $randomDate,  // ランダムな日付を使用
                 'start_time' => NULL,
                 'end_time' => NULL,
-                'location' => '場所' . $i,
                 'category_id' => NULL,
                 'created_at' => now(),
                 'updated_at' => now(),
